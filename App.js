0041c4cb86fd15a654d6ff2c8ff6fad5c46036aa
jrmccann2 <jshmccann@gmail.com>
2019-01-11 11:26:52 -0700

    initial commit w/ dependencies

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to AWS s3 Uploader
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
