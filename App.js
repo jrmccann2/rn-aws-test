25cba57b6eb1fbf44985fbc8e79a2d6f0bbfe016
jrmccann2 <jshmccann@gmail.com>
2019-01-11 12:04:52 -0700

    tutorial not compatible with Expo

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+import { RNS3 } from 'react-native-aws3';
 
 export default class App extends React.Component {
+
+  takePic(){
+    ImagePicker.showImagePicker({}, (response) => {
+      console.log(response)
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to AWS s3 Uploader
         </Text>
+        <TouchableOpacity onPress={this.takePic.bind(this)}>
+          <Text>Take Picture</Text>
+        </TouchableOpacity>
       </View>
     );
   }
